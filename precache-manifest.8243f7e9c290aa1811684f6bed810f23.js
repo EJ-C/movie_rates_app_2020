57cabb85bd1630b0b2e341b0a5b0d21e1ec4a032
EJ-C <10ngweekendofficial@gmail.com>
2020-10-11 12:40:36 +0900

    Updates

---
+++ precache-manifest.8243f7e9c290aa1811684f6bed810f23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd181f43fcef3a61298c7c600163caf",
+    "url": "/movie_rates_app_2020/index.html"
+  },
+  {
+    "revision": "9821579332f4233df14f",
+    "url": "/movie_rates_app_2020/static/css/main.1897c41e.chunk.css"
+  },
+  {
+    "revision": "b0487fefacde8610d1a2",
+    "url": "/movie_rates_app_2020/static/js/2.7129b562.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/movie_rates_app_2020/static/js/2.7129b562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9821579332f4233df14f",
+    "url": "/movie_rates_app_2020/static/js/main.d17afc16.chunk.js"
+  },
+  {
+    "revision": "0ecc91208f718d1d20d7",
+    "url": "/movie_rates_app_2020/static/js/runtime-main.eedf1133.js"
+  }
+]);
